155df5b46288c67296346cf349383f5cfd72fbbf
m0nds <elegbede.raymond@gmail.com>
2022-07-06 18:03:30 +0100

    'finished'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// index.js
+import React, {useState} from 'react'
+import {data} from './data.js'
+import "./App.css"
 
-function App() {
+const App = () => {
+
+  const [people, setPeople] = useState(data)
+
+  const removePerson = (id) => {
+    const newPeople = people.filter((person)=> id !== person.id)
+    setPeople(newPeople)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container">
+        <h2>{people.length} birthdays today</h2>
+          {
+            people.map(({id, image, name, age}) => {
+              return (
+                <div className="info" key={id}>
+                  <img src={image} alt=""/>
+                  <div className="infos">
+                    <h3>{name}</h3>
+                    <span>{age}</span>
+                  </div>
+                  <button onClick={()=> removePerson(id)}>Remove</button>
+                </div>
+              )
+            })
+          }
+        <button className="removePerson" onClick={()=> setPeople([])}>Clear All</button>
+      </div>
+    </>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: 1,
+    image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    name: "Bertie Yates",
+    age: "28 years"
+  },
+  {
+    id: 2,
+    image: "https://images.unsplash.com/photo-1509967419530-da38b4704bc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    name: "Hester Horgan",
+    age: "32 years"
+  },
+  {
+    id: 3,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxpVbP7QVPRIClFPnziMCUGK5EGV60STInWCo60KhDXm5Lo9YttonIjhcoMOTCB6KGM10&usqp=CAU",
+    name: "Heyana Guires",
+    age: "24 years"
+  },
+    {
+      id: 4,
+    image: "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWVufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    name: "Gustavo Hernandez",
+    age: "33 years"
+  },
+  {
+    id: 5,
+    image: "https://media.istockphoto.com/photos/portrait-of-a-handsome-black-man-picture-id1289461335?b=1&k=20&m=1289461335&s=170667a&w=0&h=7L30Sh0R-0JXjgqFnxupL9msH5idzcz0xZUAMB9hY_k=",
+    name: "Kendrick Lawal",
+    age: "24 years"
+  }
+]
